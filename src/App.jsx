fc8f00341dd11dd7994d984b47204ca9d446f796
Aliza Batool <alizabatool0618@gmail.com>
2026-08-04 16:11:25 +0500

    updated App.jsx, index.css, main.jsx and added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get starteddd</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div style={{ display: "flex", flexDirection:"column", backgroundColor: "whitesmoke"}}>
+      <div style={{  display: "flex", alignItems: "center", justifyContent: "space-between", marginBottom: "40px"}}>
+          <h1 style={{marginTop:"40px", marginLeft:"100px",}}>JOE.</h1>
+          <nav style={{display:"flex", gap:"20px", marginRight:"300px"}}>
+          <a className="home" href="Teaching/Speaking.html">Home</a>
+          <a className="shop" href="WebDesign.html">Shop</a>
+          <a className="categories" href="Animation.html">Categories</a>
+          <a className="about" href="Blog.html">About</a>
+          <a className="contact" href="Contact.html">Contact</a>
+          </nav>
+      <button style={{ marginRight:"50px", marginTop:"40px", color:"white",backgroundColor: "purple", borderRadius:"10px", width:"120px",
+           height:"40px"}}>Let's Talk</button>
+ </div>
 
-      <div className="ticks"></div>
+ <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <main style={{ maxWidth: "600px" }}>
+          <section>
+            <h1 style={{ marginLeft:"100px", fontSize: "50px", marginBottom: "10px" }}>Hi, I'm Joe Greyson</h1>
+            <h2 style={{  marginLeft:"100px", color: "purple", marginBottom: "20px" }}>Front-End Developer</h2>
+          </section>
+          
+          <article>
+<p style={{ marginLeft:"100px",  marginBottom: "30px" }}>
+Lorem ipsum dolor sit amet. Welcome to the homepage. Consectetur adipiscing elit.
+Inventore et officiis suscipit assumenda libero tuetur expedita eveniet, quaerat 
+quasi.
+            </p>
+          </article>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+          <div style={{  marginLeft:"100px", display: "flex", alignItems: "center", gap: "15px", flexWrap: "wrap" }}>
+            <button style={{ color: "white", backgroundColor: "purple", borderRadius: "10px", width: "120px", height: "40px", border: "none"}}>
+              Hire Me
+            </button>
+            <button style={{ color: "purple", backgroundColor: "transparent", border: "2px solid purple", borderRadius: "10px", width: "120px", height: "40px" }}>
+              See Projects
+            </button>
+             </div>
+        <div style={{ display: "flex", alignItems: "start", justifyContent: "start",marginLeft:"100px", }}> 
+  <button style={{ color: "white", backgroundColor: "purple", border: "none", borderRadius: "50%", width: "35px", height: "35px", marginTop:"100px", marginLeft:"20px", marginRight:"20px"}}>ln</button> 
+  <button style={{ color: "white", backgroundColor: "purple", border: "none", borderRadius: "50%", width: "35px", height: "35px", marginTop:"100px", marginLeft:"20px", marginRight:"20px" }}>f</button> 
+  <img src="purpleTwitter.png" alt="Twitter" style={{ width: "30px", height: "30px", marginTop:"100px", marginLeft:"20px", marginRight:"20px", objectFit:"contain"}} /> 
+</div>
+
+        </main>
+
+        
+        <div style={{ marginLeft: "40px" }}>
+          <img src="bn.jpg" alt="Joe Greyson" style={{ maxWidth: "400px", borderRadius: "10px",  marginRight:"100px",}} />
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
